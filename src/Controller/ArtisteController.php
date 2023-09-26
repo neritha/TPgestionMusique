dc5f0a08ec41596b6590e7d19c2b5647c7c0f6b9
unknown <victoria.carre78@gmail.com>
2023-09-26 11:44:43 +0200

    ajout fiche artiste

--- src/Controller/ArtisteController.php
+++ src/Controller/ArtisteController.php
@@ -2,21 +2,57 @@
 
 namespace App\Controller;
 
+use App\Entity\Artiste;
 use App\Repository\ArtisteRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Loader\Configurator\twig;
+use Twig\Environment;
+
+
 
 class ArtisteController extends AbstractController
 {
+
+        private $twig;
+
+        public function __construct(Environment $twig)
+        {
+            $this->twig = $twig;
+        }
+    
+
+    /*public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }*/
     /**
-     * @Route("/artistes", name="artiste", methodes={"GET"})
+     * @Route("/artistes", name="artistes", methods={"GET"})
      */
     public function listeArtistes(ArtisteRepository $repo): Response
     {
         $artistes = $repo->findAll();
         return $this->render('artiste/listeArtistes.html.twig', [
-            'lesAtistes' => $artistes
+            'lesArtistes' => $artistes
         ]);
     }
+    /** 
+    /**
+     * @Route("/artiste/{id}", name="ficheArtiste", methods={"GET"})
+    */
+    public function ficheArtiste(int $id, ArtisteRepository $repo): Response
+    {
+        $artiste = $repo->find($id);
+        return new Response($this->twig->render('artiste/ficheArtiste.html.twig', [
+            'leArtiste' => $artiste,
+        ]));
+    }
+    /*public function ficheArtiste(Artiste $artiste)
+    {
+        return $this->render('artiste/ficheArtiste.html.twig', [
+            'leArtiste' => $artiste
+        ]);
+        
+    }*/
 }
