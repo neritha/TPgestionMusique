203b89dadb932dd4212974736a91d0b81b3a6339
unknown <victoria.carre78@gmail.com>
2023-10-03 10:11:07 +0200

    ajout des syles

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use Faker\Factory;
 use App\Entity\Album;
+use App\Entity\Style;
 use App\Entity\Artiste;
 use App\Entity\Morceau;
 use PhpParser\Node\Stmt\Foreach_;
@@ -15,6 +16,17 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $faker=Factory::create("fr_FR");
+        $lesStyles = $this->chargeFichier("style.csv");
+
+        foreach ($lesStyles as $value) { // parcourir le tableau d'artistes | chacunes des lignes dans la varriable $value
+            $style = new Style(); 
+            $style      ->setId(intval($value[0]))
+                        ->setNom($value[1])
+                        ->setCouleur($faker->safeColorName());
+            $manager->persist($style);
+            $this->addReference("style".$style->getId(),$style);
+        }
+
 
         $lesArtistes = $this->chargeFichier("artiste.csv");
 
@@ -39,6 +51,7 @@ class AppFixtures extends Fixture
                         ->setNom($value[1])
                         ->setDate(intval($value[2]))
                         ->setImage($faker->imageUrl(640,480))
+                        ->addStyle($this->getReference("style" . $value[3]))
                         ->setArtiste($this->getReference("artiste" . $value[4]));
             $manager->persist($album);
             $this->addReference("album".$album->getId(),$album);
